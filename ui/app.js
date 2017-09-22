47a11d8209979775817c8f6a68c796fe87ebeaac
andydev <akanshul97@gmail.com>
2017-09-22 14:06:02 -0400

    Added overflow scroll to main app window

--- ui/app.js
+++ ui/app.js
@@ -27,7 +27,6 @@ const BCKSP = 'Backspace'
 // Buffers for objects in the scene
 var $donate = null
 
-<<<<<<< HEAD
 // Add handlers
 $(() => {
     // Set keydown handler
@@ -50,61 +49,6 @@ $(() => {
     // Set backspace handler
     $('#app').on('backspace', (event) => {
         $('#app > .char:last-of-type').remove()
-=======
-/**
- * Creates a new word and adds it to the app
- */
-function newWord() {
-    $word = $('<span class="word"></span>')
-    $('#app').append($word)
-}
-
-/**
- * Adds the current character to the current word
- *
- * @param char the char to add
- */
-function add(char) {
-    $word.html((i, html) => html + char)
-}
-
-/**
- * Releases the current word from it's grip on your heart and lets
- * it fade away into the light, never to be thought of again...
- */
-function fadeAway() {
-    // Add a space
-    add(' ')
-    // Fade effect
-    $word.delay(FADE_DELAY)
-         .fadeOut(FADE_TIME)
-    // Start anew
-    $word = null
-}
-
-/**
- * Quickly fades the word (deleting an error)
- */
-function quickFade() {
-    // Add a space
-    add(' ')
-    // Quick fade effect
-    $word.fadeOut(FADE_TIME)
-    $word = null
-}
-
-// Add handlers
-$(() => {
-    // Set keydown handler
-    $(document).keydown((event) => {
-        // Create a word if word is not created
-        if ($word === null) newWord()
-        // Add characters to the word
-        if (_.includes(WCHARS, event.key)) add(event.key)
-        // Fade the word if a terminator character is typed
-        if (_.includes(WRESET, event.key)) quickFade()
-        else if (_.includes(WDELIM, event.key)) fadeAway()
->>>>>>> c9d9149f75cb99fe621fd0214f38d354f89493df
     })
 
     // Reveal the donate button after a while
