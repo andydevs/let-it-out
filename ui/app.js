c9d9149f75cb99fe621fd0214f38d354f89493df
andydev <akanshul97@gmail.com>
2017-09-21 21:37:19 -0400

    Added quick fade back

--- ui/app.js
+++ ui/app.js
@@ -59,6 +59,17 @@ function fadeAway() {
     $word = null
 }
 
+/**
+ * Quickly fades the word (deleting an error)
+ */
+function quickFade() {
+    // Add a space
+    add(' ')
+    // Quick fade effect
+    $word.fadeOut(FADE_TIME)
+    $word = null
+}
+
 // Add handlers
 $(() => {
     // Set keydown handler
@@ -68,7 +79,8 @@ $(() => {
         // Add characters to the word
         if (_.includes(WCHARS, event.key)) add(event.key)
         // Fade the word if a terminator character is typed
-        if (_.includes(WDELIM, event.key)) fadeAway()
+        if (_.includes(WRESET, event.key)) quickFade()
+        else if (_.includes(WDELIM, event.key)) fadeAway()
     })
 
     // Reveal the donate button after a while
