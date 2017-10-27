41fea2bcf82d77872892e5fa250095ff3a10d5df
andydev <akanshul97@gmail.com>
2017-10-27 17:51:42 -0400

    Implemented clear button

--- ui/app.js
+++ ui/app.js
@@ -17,13 +17,16 @@ const FADE_DELAY = 180000
 const FADE_TIME  = 2000
 
 // Word information
-const CHARS = ' 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?@#$%^&*()_+-=\\/><.,;:~`"\'()[]{}'.split('')
+const CHARS = (' 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            + '!?@#$%^&*()_+-=\\/><.,;:~`"\'()[]{}').split('')
 const BCKSP = 'Backspace'
 
 // Add handlers
 $(document).ready(() => {
     // Set document keydown handler
+    console.log('Document keydown handler')
     $(document).on('keydown', (event) => {
+        console.log('#app keydown event')
         if (_.includes(CHARS, event.key))
             $('#app').trigger('writechar', [event.key])
         else if (event.key === BCKSP)
@@ -31,23 +34,35 @@ $(document).ready(() => {
     })
 
     // Set writechar handler for app
+    console.log('Special handlers for #app')
     $('#app').on('writechar', (event, chr) => {
+        console.log('#app writechar event: ' + chr)
         $('#app').append(
             $(`<span class="char">${chr}</span>`)
                 .delay(FADE_DELAY)
-                .animate({opacity: 0}, FADE_TIME)
-        )
+                .animate({opacity: 0}, FADE_TIME))
     })
-
     // Set backspace handler for app
     $('#app').on('backspace', (event) => {
+        console.log('#app backspace event')
         $('#app > .char:last-of-type').remove()
+    })
+    // Set clear handler for app
+    $('#app').on('clear', (event) => {
+        console.log('#app clear event')
+        $('#app > .char').remove()
     })
 
     // Append toolbar
+    console.log('Append toolbar...')
     $('#app').append(
         $('<div id="toolbar"></div>')
             .append('<span class="button" id="button-darkmode"></span>')
-            .append('<span class="button" id="button-clear"></span>')
-    )
+            .append('<span class="button" id="button-clear"></span>'))
+    // Set toolbar handlers
+    console.log('Set toolbar handlers')
+    $('#button-clear').on('click', (event) => {
+        console.log('#button-clear pressed')
+        $('#app').trigger('clear')
+    })
 })
